61b5801702aceb280db50a4df18e2aa49cdd3248
jdev92 <juliencavaillon6@gmail.com>
2023-03-10 16:10:18 +0100

    l'alphabet à partir de

--- terre03.rb
+++ terre03.rb
@@ -2,18 +2,23 @@
 
 # Vérifie si un argument est passé en ligne de commande
 if ARGV.empty?
-      puts "Veuillez entrer un chiffre en argument."
-else 
-# Convertit l'argument passé en ligne de commande en un chiffre
-number = ARGV[0].to_i
-# Vérifie si l'argument passé en ligne de commande est un chiffre
-      if number.to_s != ARGV[0]
-            puts "Veuillez entrer un chiffre."
-      else 
-          if number.even?
-              puts "pair"
-          else
-              puts "impair"
-          end
+    puts "Veuillez entrer une lettre en argument."
+  elsif ARGV[0].nil?
+    puts "L'argument ne doit pas être vide."
+  else
+    # Vérifie si l'argument passé est une lettre
+    if ARGV[0].match?(/[[:alpha:]]/)
+      # Récupère l'argument et le convertit en minuscule
+      first_letter = ARGV[0].downcase
+      # Calcul l'index de la première lettre dans l'alphabet et soustrait le code ASCII de la lettre
+      index_letter = first_letter.ord - 'a'.ord
+      # Tableau contenant toutes les lettres de l'alphabet à partir de la première lettre
+      alphabet = ('a'..'z').to_a[index_letter..-1] 
+      # Boucle sur chaque lettre de l'alphabet et les affichent
+      for letter in alphabet do
+        print letter
       end
+    else
+      puts "L'argument doit être une lettre"
+    end
 end
