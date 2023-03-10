bc8acab9e2f9edf8e19a863faa9776e602c4d593
jdev92 <juliencavaillon6@gmail.com>
2023-03-10 16:14:51 +0100

    pair ou impair

---
+++ terre04.rb
@@ -0,0 +1,20 @@
+#/usr/bin/env/ruby
+
+# Vérifie si un argument est passé en ligne de commande
+if ARGV.empty?
+    puts "Veuillez entrer un chiffre en argument."
+else 
+# Convertit l'argument passé en ligne de commande en un chiffre
+number = ARGV[0].to_i
+# Vérifie si l'argument passé en ligne de commande est un chiffre
+    if number.to_s != ARGV[0]
+    puts "Veuillez entrer un chiffre."
+    else 
+# Vérifie si c'est un chiffre pair ou impair    
+        if number.even?
+            puts "pair"
+        else
+            puts "impair"
+        end
+    end
+end
